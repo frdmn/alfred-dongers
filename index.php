f4aa856c9b150e0b9313bff12125b7197e8f28f1
Jonas Friedmann <j@frd.mn>
2014-11-12 12:32:07 +0100

    Refactor pageination parser

--- index.php
+++ index.php
@@ -1,30 +1,68 @@
 <?php
+// Include SimpleHTMLDOM class
 require('simplehtmldom.php');
 
+// Init empty array
+$dongers = array();
+$categories = array();
+
+/*
+ * Parse available categories
+ */
+
+// Initial request to parse available categories
 $html = file_get_html('http://dongerlist.com');
 
-$array_limit="50";
+// Store DOM in variable
+$parsed_categories = $html->find('a.list-2-anchor');
 
-// Find available pages
-$page_current = trim($html->find('.wp-pagenavi .current', 0)->plaintext);
-$page_last = trim($html->find('.wp-pagenavi .last', 0)->plaintext);
+// Push to array
+foreach ($parsed_categories as $parsed_category) {
+  // Push all elements except the "All" one to array
+  if ($parsed_category->plaintext != "All") {
+    array_push($categories, $parsed_category->plaintext);
+  }
+}
 
-// Init empty array
-$dongers[] = array();
+/*
+ * Parse dongers per category
+ */
 
-while ($page_current <= $page_last) {
-  $i = 0;
-  foreach($html->find('.list-1-item') as $donger) {
-    $plaintext = $donger->find('.donger', 0)->plaintext;
-    if ($array_limit > $i) {
-      array_push($dongers, $plaintext);
-      $i++;
-    } else {
-      echo "[INFO] Array limit exceeded (".$array_limit." > ".$i.")";
-      return 1;
+// For each available category
+foreach ($categories as $category) {
+  // Parse available pages for specific cateogory
+  $html = file_get_html('http://dongerlist.com/category/'.$category);
+
+  // Find available pages
+  $page_current = $html->find('.wp-pagenavi .current', 0);
+  $page_last = $html->find('.wp-pagenavi .last', 0);
+
+  // Check if pagination available
+  if ($page_current && $page_last) {
+    // While theres a new page available
+    while ($page_current <= $page_last) {
+    // Pagination found => multiple pages for that specific category
+    $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page_current);
+      foreach($html->find('.list-1-item') as $donger) {
+        // Create tiny array per each donger to save meta data like category
+        $item['donger'] = $donger->find('.donger', 0)->plaintext;
+        $item['category'] = $category;
+        // Push to $dongers[] array
+        array_push($dongers, $item);
+      }
+      // Continue with next page
+      $page_current++;
+    }
+  } else {
+    // No pagination => only one page of dongers
+    foreach($html->find('.list-1-item') as $donger) {
+        // Create tiny array per each donger to save meta data like category
+        $item['donger'] = $donger->find('.donger', 0)->plaintext;
+        $item['category'] = $category;
+        // Push to $dongers[] array
+        array_push($dongers, $item);
     }
   }
-  $page_current++;
 }
 
 var_dump($dongers);

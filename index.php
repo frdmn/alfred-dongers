b78c4468d3f4e99d414ba4a38004a4fca13c0e5b
Jonas Friedmann <j@frd.mn>
2014-11-12 11:46:13 +0100

    Parse all available pages

--- index.php
+++ index.php
@@ -9,21 +9,22 @@ $array_limit="50";
 $page_current = trim($html->find('.wp-pagenavi .current', 0)->plaintext);
 $page_last = trim($html->find('.wp-pagenavi .last', 0)->plaintext);
 
-$html = file_get_html('http://dongerlist.com/page/'.$page_random);
-
 // Init empty array
 $dongers[] = array();
-$i = 0;
 
-foreach($html->find('.list-1-item') as $donger) {
-  $plaintext = $donger->find('.donger', 0)->plaintext;
-  if ($array_limit > $i) {
-    array_push($dongers, $plaintext);
-    $i++;
-  } else {
-    echo "[INFO] Array limit exceeded (".$array_limit." > ".$i.")";
-    return 1;
+while ($page_current <= $page_last) {
+  $i = 0;
+  foreach($html->find('.list-1-item') as $donger) {
+    $plaintext = $donger->find('.donger', 0)->plaintext;
+    if ($array_limit > $i) {
+      array_push($dongers, $plaintext);
+      $i++;
+    } else {
+      echo "[INFO] Array limit exceeded (".$array_limit." > ".$i.")";
+      return 1;
+    }
   }
+  $page_current++;
 }
 
 var_dump($dongers);

32eb2dc6a182615c415231524bda52efc0fab83f
Jonas Friedmann <j@frd.mn>
2014-11-13 18:15:48 +0100

    Wrapper base

---
+++ donger.php
@@ -0,0 +1,54 @@
+<?php
+
+// Include Alfred PHP lib
+require_once('workflow.class.php');
+
+// Function to render the result XML
+function render_results($input_array){
+  $i = 1;
+  $w = new Workflows();
+  $results = array();
+  foreach ($input_array as $input_item) {
+    $temp = array(
+        'uid'          => $i,
+        'title'        => $input_item,
+    );
+    array_push($results, $temp);
+    $i++;
+  }
+  // Return XML
+  print $w->toXML($results);
+}
+
+// Read and store cached dongers
+$donger_raw = json_decode(file_get_contents($_SERVER['HOME']."/.donger.cache"));
+// Initalize some empty arrays
+$catgories = array();
+$dongers = array();
+
+// Check for arguments (show only specified category or list categories)
+if (isset($argv[1])) {
+  // If argument is "list" => list categories
+  if ($argv[1] == "list") {
+    foreach ($donger_raw as $donger) {
+      $catgories[] = $donger->category;
+    }
+    render_results(array_unique($catgories));
+  // If argument is "category" => show dongers of specific category
+  } else {
+    foreach ($donger_raw as $donger) {
+      if (strtolower($argv[1]) == $donger->category) {
+        $dongers[] = $donger->donger;
+      }
+    }
+    render_results($dongers);
+  }
+} else {
+  // No arguments => show all dongers
+  foreach ($donger_raw as $donger) {
+    $dongers[] = $donger->donger;
+  }
+  render_results($dongers);
+}
+
+?>

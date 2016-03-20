c2293bf3eeef7b08a29bbc3dd6b1cd86ea056520
Geng-G-Razor <594030667@qq.com>
2016-03-21 00:14:44 +0800

    bug fix for can not download happy && donger categories

--- parser/parser.php
+++ parser/parser.php
@@ -11,7 +11,7 @@ $categories = array();
  */
 
 // Initial request to parse available categories
-$html = file_get_html('http://dongerlist.com');
+$html = file_get_html('http://dongerlist.com/category/happy');
 
 // Store DOM in variable
 $parsed_categories = $html->find('a.list-2-anchor');
@@ -35,14 +35,15 @@ foreach ($categories as $category) {
 
   // Find available pages
   $page_current = $html->find('.wp-pagenavi .current', 0);
-  $page_last = $html->find('.wp-pagenavi .last', 0);
-
+  $page_last = strip_tags($html->find('.wp-pagenavi .last', 0));
+  echo $page_last;
+  $page = 1;
   // Check if pagination available
   if ($page_current && $page_last) {
     // While theres a new page available
-    while ($page_current <= $page_last) {
+    while ($page <= $page_last) {
     // Pagination found => multiple pages for that specific category
-    $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page_current);
+    $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
       foreach($html->find('.list-1-item') as $donger) {
         // Create tiny array per each donger to save meta data like category
         $item['donger'] = $donger->find('.donger', 0)->plaintext;
@@ -51,7 +52,7 @@ foreach ($categories as $category) {
         array_push($dongers, $item);
       }
       // Continue with next page
-      $page_current++;
+      $page++;
     }
   } else {
     // No pagination => only one page of dongers

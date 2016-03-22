d0ced646e6a29e44560a6b29a182e40543137003
Jonas Friedmann <j@frd.mn>
2016-03-22 11:10:56 +0100

    Reindent lines

--- parser/parser.php
+++ parser/parser.php
@@ -19,8 +19,8 @@ $parsed_categories = $html->find('a.list-2-anchor');
 // Push to array
 foreach ($parsed_categories as $parsed_category) {
     // Push all elements except the "All" one to array
-    if ($parsed_category->plaintext != "All") {
-        array_push($categories, $parsed_category->plaintext);
+  if ($parsed_category->plaintext != "All") {
+    array_push($categories, $parsed_category->plaintext);
   }
 }
 
@@ -30,51 +30,52 @@ foreach ($parsed_categories as $parsed_category) {
 
 // For each available category
 foreach ($categories as $category) {
-    // Parse available pages for specific cateogory
-    $html = file_get_html('http://dongerlist.com/category/'.$category);
+  // Parse available pages for specific cateogory
+  $html = file_get_html('http://dongerlist.com/category/'.$category);
 
-    // Find available pages
-    $page = 1;
-    $page_last = strip_tags($html->find('.wp-pagenavi .last', 0));
-    // Check if pagination available
-    if ($page_last) {
-        // While theres a new page available
-        while ($page <= $page_last) {
-            // Pagination found => multiple pages for that specific category
-            $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
-            foreach($html->find('.list-1-item') as $donger) {
-                // Create tiny array per each donger to save meta data like category
-                $item['donger'] = $donger->find('.donger', 0)->plaintext;
-                $item['category'] = strtolower($category);
-                // Push to $dongers[] array
-                array_push($dongers, $item);
+  // Find available pages
+  $page = 1;
+  $page_last = strip_tags($html->find('.wp-pagenavi .last', 0));
+  // Check if pagination available
+  if ($page_last) {
+    // While theres a new page available
+    while ($page <= $page_last) {
+      // Pagination found => multiple pages for that specific category
+      $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
+      foreach($html->find('.list-1-item') as $donger) {
+        // Create tiny array per each donger to save meta data like category
+        $item['donger'] = $donger->find('.donger', 0)->plaintext;
+        $item['category'] = strtolower($category);
+        // Push to $dongers[] array
+        array_push($dongers, $item);
       }
       // Continue with next page
       $page++;
     }
   } else {
-      $page_larg = strip_tags($html->find('.wp-pagenavi .larger', 0));
-      while ($page_larg){
-              // Pagination found => multiple pages for that specific category
-              $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
-              foreach($html->find('.list-1-item') as $donger) {
-                  // Create tiny array per each donger to save meta data like category
-                  $item['donger'] = $donger->find('.donger', 0)->plaintext;
-                  $item['category'] = strtolower($category);
-                  // Push to $dongers[] array
-                  array_push($dongers, $item);
+    $page_larg = strip_tags($html->find('.wp-pagenavi .larger', 0));
+    while ($page_larg){
+      // Pagination found => multiple pages for that specific category
+      $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
+      foreach($html->find('.list-1-item') as $donger) {
+        // Create tiny array per each donger to save meta data like category
+        $item['donger'] = $donger->find('.donger', 0)->plaintext;
+        $item['category'] = strtolower($category);
+        // Push to $dongers[] array
+        array_push($dongers, $item);
       }
       // Continue with next page
       $page_larg = strip_tags($html->find('.wp-pagenavi .larger', 0));
       $page++;
-        }
-            // No pagination => only one page of dongers
-            foreach($html->find('.list-1-item') as $donger) {
-                // Create tiny array per each donger to save meta data like category
-                $item['donger'] = $donger->find('.donger', 0)->plaintext;
-                $item['category'] = strtolower($category);
-                // Push to $dongers[] array
-                array_push($dongers, $item);
+    }
+
+    // No pagination => only one page of dongers
+    foreach($html->find('.list-1-item') as $donger) {
+      // Create tiny array per each donger to save meta data like category
+      $item['donger'] = $donger->find('.donger', 0)->plaintext;
+      $item['category'] = strtolower($category);
+      // Push to $dongers[] array
+      array_push($dongers, $item);
     }
   }
 }

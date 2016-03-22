7f33ccef6b30d7d6806c87356ec2c0fade91faea
Jonas Friedmann <j@frd.mn>
2016-03-22 11:49:31 +0100

    Refactor parser and remove unused code parts

--- parser/parser.php
+++ parser/parser.php
@@ -5,6 +5,20 @@ require('simplehtmldom.php');
 // Init empty array
 $dongers = array();
 $categories = array();
+$debug = false;
+
+/**
+ * Function to optionally output debug messages
+ * @param  {String} $msg input message
+ * @return void
+ */
+function debug($msg){
+  global $debug;
+
+  if ($debug) {
+    echo($msg);
+  }
+}
 
 /*
  * Parse available categories
@@ -18,7 +32,7 @@ $parsed_categories = $html->find('a.list-2-anchor');
 
 // Push to array
 foreach ($parsed_categories as $parsed_category) {
-    // Push all elements except the "All" one to array
+  // Push all elements except the "All" one to array
   if ($parsed_category->plaintext != "All") {
     array_push($categories, $parsed_category->plaintext);
   }
@@ -30,46 +44,28 @@ foreach ($parsed_categories as $parsed_category) {
 
 // For each available category
 foreach ($categories as $category) {
+  debug('Starting to parse category "'. $category."\"\r\n");
   // Parse available pages for specific cateogory
   $html = file_get_html('http://dongerlist.com/category/'.$category);
 
-  // Find available pages
-  $page = 1;
-  $page_last = strip_tags($html->find('.wp-pagenavi .last', 0));
-  // Check if pagination available
-  if ($page_last) {
-    // While theres a new page available
-    while ($page <= $page_last) {
-      // Pagination found => multiple pages for that specific category
-      $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
-      foreach($html->find('.list-1-item') as $donger) {
-        // Create tiny array per each donger to save meta data like category
-        $item['donger'] = $donger->find('.donger', 0)->plaintext;
-        $item['category'] = strtolower($category);
-        // Push to $dongers[] array
-        array_push($dongers, $item);
-      }
-      // Continue with next page
-      $page++;
-    }
-  } else {
-    $page_larg = strip_tags($html->find('.wp-pagenavi .larger', 0));
-    while ($page_larg){
-      // Pagination found => multiple pages for that specific category
-      $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
-      foreach($html->find('.list-1-item') as $donger) {
-        // Create tiny array per each donger to save meta data like category
-        $item['donger'] = $donger->find('.donger', 0)->plaintext;
-        $item['category'] = strtolower($category);
-        // Push to $dongers[] array
-        array_push($dongers, $item);
-      }
-      // Continue with next page
-      $page_larg = strip_tags($html->find('.wp-pagenavi .larger', 0));
-      $page++;
-    }
+  // Parse avaiable pagination infos and store current and last page in variable
+  $page = trim($html->find('.wp-pagenavi .current', 0)->plaintext);
+  $pages = $html->find('.wp-pagenavi .page');
 
-    // No pagination => only one page of dongers
+  // If pages contain elements ...
+  if ($pages) {
+    // ... use the last one as last page
+    $last_page = trim(end($pages)->plaintext);
+  } else {
+    // ... otherwise, there are no additional pages, use the current page as last page
+    $last_page = $page;
+  }
+
+  // As long as we didn't reach last page
+  while ($page <= $last_page) {
+    debug('- parsing page '.$page.'/'.$last_page."\n");
+    $html = file_get_html('http://dongerlist.com/category/'.$category.'/page/'.$page);
+    // Search each donger on the current page
     foreach($html->find('.list-1-item') as $donger) {
       // Create tiny array per each donger to save meta data like category
       $item['donger'] = $donger->find('.donger', 0)->plaintext;
@@ -77,6 +73,8 @@ foreach ($categories as $category) {
       // Push to $dongers[] array
       array_push($dongers, $item);
     }
+    // Continue with next page
+    $page++;
   }
 }
 
